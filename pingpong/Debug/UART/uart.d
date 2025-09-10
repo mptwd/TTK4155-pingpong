69138813bae5466b174e9c80a18618d1c6a1a3ba
Marc John Portwood <marcjpo@ntnu.no>
2025-09-10 18:26:21 +0200

    Milestone 1 works !

--- pingpong/Debug/UART/uart.d
+++ pingpong/Debug/UART/uart.d
@@ -1,4 +1,4 @@
-main.d main.o: .././main.c \
+UART/uart.d UART/uart.o: ../UART/uart.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
@@ -10,14 +10,11 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././uart/uart.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../UART/uart.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -41,18 +38,12 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
-
-.././uart/uart.h:
+../UART/uart.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
--- pingpong/main.c
+++ pingpong/main.c
@@ -8,17 +8,48 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "uart/uart.h"
+#include <avr/interrupt.h>
+
 int main(void) {
-	// Configuration de la sortie (PORTB pin0)
-	DDRB |= (1 << PB0);
-
+	/*
+	uart_init(9600);
+	sei();
+	
+	printf("Hello UART! %d %s\n", 123, "test");
+	
 	while (1) {
-		// Allume la led (High)
-		PORTB |= (1 << PB0);
+		uint8_t data;
+		if (uart_receive_byte(&data)) {
+			printf("You typed: %c\n", data);
+		}
+	}
+	*/
+	/*
+	// Port A as output
+	DDRA = 0xFF;
+	
+	// ALE pin as output
+	DDRE |= (1 << PE1);
+	
+	while(1) {
+		PORTA = 0x55;
+		PORTE |= (1 << PE1);
+		_delay_ms(1);
+		PORTE &= !(1 << PE1);
 		_delay_ms(500);
-
-		// Eteint la led (Low)
-		PORTB &= ~(1 << PB0);
+		
+		PORTA = 0xAA;
+		PORTE |= (1 << PE1);
+		_delay_ms(1);
+		PORTE &= !(1 << PE1);
 		_delay_ms(500);
 	}
+	*/
+	volatile uint8_t *ext_ram = (uint8_t)0x8000;
+	while(1) {
+			
+	ext_ram[2] = 0x55;
+	}
+
 }
